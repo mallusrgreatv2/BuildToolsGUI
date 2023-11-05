5d160176345e70654bb3a730f042890ff3fd6198
mallusrgreatv2 <freedomgamingyt@gmail.com>
2023-11-05 17:35:05 +0530

    Updated versions

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // Modules
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 
 // Create window after initialization
-app.whenReady().then(function() {
-    const win = new BrowserWindow({
-        width: 600,
-        height: 790,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-
-    win.removeMenu();
-    win.loadFile("index.html");
+app.whenReady().then(function () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 1580,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+  win.removeMenu();
+  win.loadFile("index.html");
 });
 
 // Quit when all windows are closed
-app.on("window-all-closed", function() {
-    app.quit();
+app.on("window-all-closed", function () {
+  app.quit();
 });
